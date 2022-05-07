eea753ca7e94c41bf5bd3a6a85c0c0dbc8be0cc3
janzietek <jzietek@student.agh.edu.pl>
2022-05-07 13:55:08 +0200

    net fixing

--- projekt_2/net.py
+++ projekt_2/net.py
@@ -2,69 +2,55 @@ import numpy as np
 from random import randint, shuffle
 
 
-class InvalidWeightsException(Exception):
-    pass
-
-
-class InvalidNetworkInputException(Exception):
-    pass
-
-
 class HopfieldNetwork(object):
-    def __init__(self, num_inputs):
-        self._num_inputs = num_inputs
-        self._weights = np.random.uniform(-1.0, 1.0, (num_inputs, num_inputs))
+    def __init__(self, neurons_num):
+        self._neutrons_num = neurons_num
+        self._weights = np.random.uniform(-1.0, 1.0, (neurons_num, neurons_num))
 
     def set_weights(self, weights):
-        """Update the weights array"""
-        if weights.shape != (self._num_inputs, self._num_inputs):
-            raise InvalidWeightsException()
-
         self._weights = weights
 
     def get_weights(self):
-        """Return the weights array"""
         return self._weights
 
-    def calculate_neuron_output(self, neuron, input_pattern):
-        """Calculate the output of the given neuron"""
-        num_neurons = len(input_pattern)
+    def calculate_neuron_output(self, neuron, pattern):
+        neurons_num = len(pattern)
 
         s = 0.0
 
-        for j in range(num_neurons):
-            s += self._weights[neuron][j] * input_pattern[j]
+        for j in range(neurons_num):
+            s += self._weights[neuron][j] * pattern[j]
 
-        return 1.0 if s > 0.0 else -1.0
+        res = 1.0 if s > 0.0 else -1.0
 
-    def run_once(self, update_list, input_pattern):
-        """Iterate over every neuron and update it's output"""
-        result = input_pattern.copy()
+        return res
 
-        changed = False
-        for neuron in update_list:
-            neuron_output = self.calculate_neuron_output(neuron, result)
+    def run(self, pattern, max_iterations=10):
+        counter = 0
 
-            if neuron_output != result[neuron]:
-                result[neuron] = neuron_output
-                changed = True
-
-        return changed, result
-
-    def run(self, input_pattern, max_iterations=10):
-        """Run the network using the input data until the output state doesn't change
-        or a maximum number of iteration has been reached."""
-        iteration_count = 0
-
-        result = input_pattern.copy()
+        res = pattern.copy()
 
         while True:
-            update_list = range(self._num_inputs)
-            shuffle(list(update_list))
+            update = range(self._neutrons_num)
+            shuffle(list(update))
 
-            changed, result = self.run_once(update_list, result)
+            changed, res = self.run_once(update, res)
 
-            iteration_count += 1
+            counter += 1
 
-            if not changed or iteration_count == max_iterations:
-                return result
+            if not changed or counter == max_iterations:
+                return res
+
+        
+    def run_once(self, update, pattern):
+        res = pattern.copy()
+
+        flag = False
+        for n in update:
+            n_output = self.calculate_neuron_output(n, res)
+            if n_output != res[n]:
+                flag = True
+                res[n] = n_output
+
+        return flag, res
+
