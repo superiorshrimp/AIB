1e9e8f5d467f9cfb4eb20c0dc1386ee122972e29
janzietek <jzietek@student.agh.edu.pl>
2022-05-07 14:00:20 +0200

    train fixing

--- projekt_2/trainers.py
+++ projekt_2/trainers.py
@@ -1,40 +1,23 @@
 import numpy as np
 
 
-def calculate_weight(i, j, patterns):
-    """Calculate the weight between the given neurons"""
-    num_patterns = len(patterns)
+def calc_wg_of_neuron(neuron_idx, input_models):
+    wg = np.zeros(len(input_models[0]))
     
-    s = 0.0
-    for mu in range(num_patterns):
-        s += patterns[mu][i] * patterns[mu][j]
-    
-    w = (1.0 / float(num_patterns)) * s
+    for j in range(len(input_models[0])):
+        if neuron_idx == j: continue
+        sum = 0.0
+        for m in range(len(input_models)):
+            sum += input_models[m][neuron_idx] * input_models[m][j]
+        
+        wg[j] = (1.0 / float(len(input_models))) * sum
 
-    return w
+    return wg
 
-def calculate_neuron_weights(neuron_index, input_patterns):
-    """Calculate the weights for the givven neuron"""
-    num_patterns = len(input_patterns)
-    num_neurons = len(input_patterns[0])
+def train(network, input_models):
+    weights = np.zeros((len(input_models[0]), len(input_models[0])))
 
-    weights = np.zeros(num_neurons)
-
-    for j in range(num_neurons):
-        if neuron_index == j: continue
-        weights[j] = calculate_weight(neuron_index, j, input_patterns)
-
-    return weights
-
-def hebbian_training(network, input_patterns):
-    """Train a network using the Hebbian learning rule"""
-    n = len(input_patterns)
-
-    num_neurons = len(input_patterns[0])
-
-    weights = np.zeros((num_neurons, num_neurons))
-
-    for i in range(num_neurons):
-        weights[i] = calculate_neuron_weights(i, input_patterns)
+    for i in range(len(input_models[0])):
+        weights[i] = calc_wg_of_neuron(i, input_models)
 
     network.set_weights(weights)
