0ebc177915805ba1ddc8c1b257b82622eb6e4c92
superiorshrimp <szymon.zychowicz300701@gmail.com>
2022-05-07 14:49:51 +0200

    a

--- projekt_2/trainers.py
+++ projekt_2/trainers.py
@@ -5,7 +5,6 @@ def calc_wg_of_neuron(neuron_idx, input_models):
     wg = np.zeros(len(input_models[0]))
     
     for j in range(len(input_models[0])):
-        if neuron_idx == j: continue
         sum = 0.0
         for m in range(len(input_models)):
             sum += input_models[m][neuron_idx] * input_models[m][j]
